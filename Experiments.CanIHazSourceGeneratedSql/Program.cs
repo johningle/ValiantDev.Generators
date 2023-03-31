0e40739cc98f450a024927d7b2af688d1edd05c6
John Ingle <ingle.john.w@gmail.com>
2023-03-31 17:59:55 -0500

    Refactored to generate source with Roslyn syntax objects.

--- Experiments.CanIHazSourceGeneratedSql/Program.cs
+++ Experiments.CanIHazSourceGeneratedSql/Program.cs
@@ -1,27 +1,11 @@
-﻿using System.Reflection;
-using Generators;
-
-namespace Experiments.CanIHazSourceGeneratedSql;
+﻿namespace Experiments.CanIHazSourceGeneratedSql;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        foreach (var member in typeof(SqlFiles).GetFields(BindingFlags.Static | BindingFlags.Public))
-        {
-            var name = member.Name;
-            var value = member.GetValue(null)!.ToString();
-            Console.WriteLine($"{Environment.NewLine}{name}: {value}");
-        }
-
         // example of direct usage
-        var someSql = SqlFiles.TestQuery;
-
-        /*
-        // debug generator
-        var generator = new CodeGenerator();
-        generator.AddSqlMember("foo", "bar");
-        Console.WriteLine(generator.GetSourceCode());
-        */
+        var testQuery = SqlFiles.TestQuery;
+        var testAnotherQuery = SqlFiles.Repositories.TestAnotherQuery;
     }
 }
